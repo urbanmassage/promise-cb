8998bb9af2c1690afa3eabcb7affc1ff23d873c8
Louay Alakkad <louay@alakkad.me>
2016-04-25 16:20:25 +0100

    Accept PromiseLike instead of Promise in toCallback

--- to.ts
+++ to.ts
@@ -2,11 +2,11 @@ interface Callback<T> {
   (err: Error, result: T): void;
 }
 
-function toCallback<T>(promise: Promise<T>, cb: Callback<T>): void {
+function toCallback<T>(promise: PromiseLike<T>, cb: Callback<T>): void {
   promise.then(
     result => cb(null, result),
     err => cb(err, null)
-  ).catch(err => {
+  ).then(null, err => {
     // Don't swallow thrown errors
     setTimeout(() => {
       throw err;
