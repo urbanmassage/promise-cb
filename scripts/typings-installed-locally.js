34c341e190b9b53a800801a1b52abe7e66094742
Louy <louy08@gmail.com>
2016-10-27 22:53:49 +0100

    Remove postinstall script

--- scripts/typings-installed-locally.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var path = require('path');
-try {
-  fs.statSync(path.join(__dirname, '../node_modules/.bin/typings' + (process.platform === 'win32' ? '.cmd' : '')));
-} catch(e) {
-  process.exit(1);
-}
